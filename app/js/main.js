286aec527230315ac2c62e26396dd175ce563825
Tatiana Osipova <otv120780@gmail.com>
2023-04-08 15:50:19 -0400

    feat: start styling section top

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,9 @@ $(function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });   
     
-    $('.top__slider').slick({});
+    $('.top__slider').slick({
+        dots: true,
+        arrow: false,
+    });
 });
 
